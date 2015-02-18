3984d6fd4a57c3e60b79f18890114b3172078d9a
Lew Gordon <lew.gordon@inin.com>
2015-02-17 21:25:10 -0500

    Initial commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,33 @@
 package controllers
 
+import java.sql.ResultSet
+
+import play.api.Play.current
+import play.api.db.DB
+import play.api.libs.json._
 import play.api.mvc._
 
 object Application extends Controller {
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index(query: Option[String]) = Action {
+    val conn = DB.getConnection()
+    var allPokemon: JsArray = JsArray()
+    try {
+      val stmt = conn.createStatement
+      val rs : ResultSet = stmt.executeQuery("SELECT * FROM pokemon")
+      while (rs.next()) {
+        allPokemon :+= getPokemonJson(rs)
+      }
+    } finally {
+      conn.close()
+    }
+    Ok(Json.prettyPrint(allPokemon))
   }
 
+  def getPokemonJson(rs: ResultSet): JsValue = {
+    JsObject(Seq(
+      "number" -> JsNumber(rs.getInt("number")),
+      "name" -> JsString(rs.getString("name")), 
+      "weight" -> JsNumber(rs.getDouble("weight"))
+    ))
+  }
 }
--- build.sbt
+++ build.sbt
@@ -8,4 +8,6 @@ scalaVersion := "2.11.1"
 
 libraryDependencies ++= Seq( jdbc , anorm , cache , ws )
 
+libraryDependencies += "org.postgresql" % "postgresql" % "9.4-1200-jdbc41"
+
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
